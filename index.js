15f509c29c9533ad0711225b2399cff6f1b5664e
Ghiftee <gigibetine@gmail.com>
2022-11-10 10:27:56 +0100

    Implement edit button

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ function onSubmitForm(e){
     showAllTodo(formInputValue)
 }
 
+function editTodo(e){
+    const clickedEditButton = e.target;
+    const spanElement = clickedEditButton.parentNode.parentNode.children[1];
+    const inputElement = clickedEditButton.parentNode.parentNode.children[2];
+    
+    if (e.target.innerHTML === 'Edit'){
+        clickedEditButton.innerHTML = 'Save';
+        inputElement.hidden = false;
+        spanElement.hidden = true;
+        inputElement.focus();
+    }else{
+        spanElement.innerHTML = inputElement.value;
+        spanElement.hidden = false;
+        inputElement.hidden = true;
+        clickedEditButton.innerHTML = 'Edit';
+    }
+}
+
+function addListenerToEdit(){
+    const allEditButtons = document.querySelectorAll('.edit-btn')
+    const lastEditButton = allEditButtons[allEditButtons.length - 1];
+    lastEditButton.addEventListener('click', editTodo);
+}
+
 function deleteTodo(e){
     const clickedDelButton = e.target;
     const entireLiPerTodo = clickedDelButton.parentNode.parentNode.parentNode
@@ -13,6 +37,7 @@ function deleteTodo(e){
     entireLiPerTodo.remove();
 }
 
+// This is called because it adds event listener to delete
 function addListenerToDelete(){
     const allDeleteButtons = document.querySelectorAll('.del-btn')
     const lastDelButton = allDeleteButtons[allDeleteButtons.length - 1];
@@ -25,6 +50,7 @@ function showAllTodo(TodoItem){
         <div class="todo-list-item">
             <input type="checkbox" class="snd-input">
             <span>${TodoItem}</span>
+            <input class="edit-todo-input" value=${TodoItem} hidden/>
             <div class="todo-btn">
                 <button id="editbtn"  class="edit-btn pdn-border">Edit</button>
                 <button id="deletebtn" class="del-btn pdn-border">Delete</button>
@@ -34,6 +60,7 @@ function showAllTodo(TodoItem){
     const newListContainer = document.getElementById('todo-ul');
     newListContainer.appendChild(newList);
 
+    addListenerToEdit();
     addListenerToDelete();
 }
 
